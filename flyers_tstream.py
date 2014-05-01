d37325b4dac7c866a743180c6becf9f02410cfc1
Daniel Forsyth <danielforsyth@Daniels-MacBook-Pro.local>
2014-05-01 12:55:24 -0400

    NHL Twitter Stream
    Collect real time stream of tweets into mongodb

---
+++ flyers_tstream.py
@@ -0,0 +1,55 @@
+
+import tweepy
+import sys
+import pymongo
+
+
+# Go to http://dev.twitter.com and create an app.
+# The consumer key and secret will be generated for you after
+consumer_key=""
+consumer_secret=""
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+access_token=""
+access_token_secret=""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+
+class CustomStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        super(tweepy.StreamListener, self).__init__()
+
+        self.db = pymongo.MongoClient().Flyers
+
+
+
+
+
+
+    def on_status(self, status):
+        print status.text , "\n"
+
+        data ={}
+        data['text'] = status.text
+        data['created_at'] = status.created_at
+        data['geo'] = status.geo
+        data['source'] = status.source
+      
+        self.db.Tweets.insert(data)
+
+    def on_error(self, status_code):
+        print >> sys.stderr, 'Encountered error with status code:', status_code
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        print >> sys.stderr, 'Timeout...'
+        return True # Don't kill the stream
+
+sapi = tweepy.streaming.Stream(auth, CustomStreamListener(api))
+sapi.filter(track=['flyers'])
